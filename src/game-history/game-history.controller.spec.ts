7c5c24d1dcc51c8b2d9ab44cad312732d5f52215
Ardhito Nurhadyansah <ardhito.nurhadyansah@ui.ac.id>
2024-03-08 18:37:37 +0700

    [RED] add mock test for game history controller

---
+++ src/game-history/game-history.controller.spec.ts
@@ -0,0 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GameHistoryController } from './game-history.controller';
+import { GameType } from '@prisma/client';
+import { GameHistoryService } from './game-history.service';
+
+const mockGameHistoryService = {
+  create: jest.fn((dto) => {
+    return { id: Date.now(), ...dto };
+  }),
+};
+
+describe('GameHistoryController', () => {
+  let controller: GameHistoryController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [GameHistoryController],
+      providers: [
+        {
+          provide: GameHistoryService,
+          useValue: mockGameHistoryService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<GameHistoryController>(GameHistoryController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new game history record', async () => {
+      const gameHistoryDto: CreateGameHistoryDto = {
+        gameType: GameType.PUZZLE,
+        score: 100,
+        startTime: new Date(),
+        endTime: new Date(),
+        userId: 1,
+      };
+
+      await expect(controller.create(gameHistoryDto)).resolves.toEqual({
+        id: expect.any(Number),
+        ...gameHistoryDto,
+      });
+
+      expect(mockGameHistoryService.create).toHaveBeenCalledWith(
+        gameHistoryDto,
+      );
+    });
+  });
+});
