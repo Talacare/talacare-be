97113fadf29c7fb54f8bb6d635c9e3a18c88115c
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-12 23:49:00 +0700

    [GREEN] Implemented get schedule controller

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -2,20 +2,27 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   HttpCode,
   HttpStatus,
   Param,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { CreateScheduleDto } from './dto/create-schedule-dto';
 import { Schedule } from '@prisma/client';
+import { GetScheduleQueryDTO } from './dto/get-schedule.dto';
+import { ResponseUtil } from '../common/utils/response.util';
 
 @Controller('schedule')
 export class ScheduleController {
-  constructor(private readonly scheduleService: ScheduleService) {}
+  constructor(
+    private readonly scheduleService: ScheduleService,
+    private readonly responseUtil: ResponseUtil,
+  ) {}
 
-  @Post('create')
+  @Post('')
   @HttpCode(HttpStatus.CREATED)
   async create(
     @Body() createScheduleDto: CreateScheduleDto,
@@ -26,6 +33,16 @@ export class ScheduleController {
   @Delete(':id')
   @HttpCode(HttpStatus.OK)
   async delete(@Param('id') id: string): Promise<void> {
-    return this.scheduleService.delete(id);
+    await this.scheduleService.delete(id);
+    return this.responseUtil.response({
+      responseMessage: 'Data deleted successfully',
+    });
+  }
+
+  @Get('')
+  @HttpCode(HttpStatus.OK)
+  async getAll(@Query() query: GetScheduleQueryDTO): Promise<Schedule[]> {
+    const schedules = await this.scheduleService.getAll(query);
+    return this.responseUtil.response({}, { data: schedules });
   }
 }
