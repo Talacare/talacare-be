a638faf16e9d683f0f2e68664eb6ab348a3393d0
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-12 16:35:30 +0700

    [GREEN] Implemented get schedule service

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateScheduleInput } from './interfaces/create-schedule-input.interface';
 import { CreateSchedule } from './interfaces/create-schedule.interface';
+import { GetScheduleQueryDTO } from './dto/get-schedule.dto';
+import { Schedule } from '@prisma/client';
 
 @Injectable()
 export class ScheduleService {
   constructor(private prisma: PrismaService) {}
 
-  public async create(scheduleData: CreateScheduleInput): Promise<CreateSchedule> {
-    return this.prisma.schedule.create({
+  async create(scheduleData: CreateScheduleInput): Promise<CreateSchedule> {
+    return await this.prisma.schedule.create({
       data: scheduleData,
     });
+  }
+
+  async getAll({ page }: GetScheduleQueryDTO): Promise<Schedule[]> {
+    const LIMIT = 10
+    const startIndex = (parseInt(page) - 1) * LIMIT;
+    const endIndex = startIndex + LIMIT;
+
+    const schedules = await this.prisma.schedule.findMany()
+    return schedules.slice(startIndex, endIndex)
   }
 }
