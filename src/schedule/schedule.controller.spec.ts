f4cce2601a27960d41ff24d9a1dbbb93fc0f4dc6
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-12 23:44:45 +0700

    [RED] Added comprehensive tests for get schedule controller
    Pada commit ini, saya mengimplementasikan unit test untuk fungsionalitas get all schedule controller yang meliputi positive, negative, dan corner cases.
    
    Positive Case:
    Test case: should get all schedules for a valid page
    Description: Test ini memvalidasi untuk kodisi query page yang valid, yakni berupa angka positif.
    Expected behavior: Respons yang diterima berupa object yang terdiri atas response code, response message, response status, dan data schedule.
    
    Negative Cases:
    Test case: should return an empty array for a page beyond the total number of pages
    Description: Test ini memvalidasi kondisi ketika query page yang diberikan terlampau besar melebihi total page yang ada.
    Expected behavior: Respons yang diterima berupa object yang terdiri atas response code, response message, response status, dan data schedule yang kosong.
    
    Test case: should return an error for invalid query parameters (page is not a number)
    Description: Test ini memvalidasi kondisi ketika query page bukan merupakan angka (misalnya berupa huruf).
    Expected behavior: Respons yang diterima adalah BadRequestException karena query param tidak sesuai.
    
    Test case: should return an error for invalid query parameters (page is not a positive number)
    Description: Test ini memvalidasi kondisi ketika query page bukan merupakan angka yang positif
    Expected behavior: Respons yang diterima adalah BadRequestException karena query param tidak sesuai.
    
    Corner Case:
    Test case: should return an error if an unexpected error occurs in the service
    Description: Test ini memvalidasi ketika kondisi tidak diduga terjadi, misal terjadi unexpected error.
    Expected behavior: Respons yang diterima merupakan error yang di-throw oleh controller.
    
    Melalui tes-tes di atas, harapannya kode akan lebih terjaga dan dapat dipercaya.

--- src/schedule/schedule.controller.spec.ts
+++ src/schedule/schedule.controller.spec.ts
@@ -2,16 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ScheduleController } from './schedule.controller';
 import { ScheduleService } from './schedule.service';
 import { CreateScheduleDto } from './dto/create-schedule-dto';
+import { GetScheduleQueryDTO } from './dto/get-schedule.dto';
+import { Schedule } from '@prisma/client';
+import { ResponseUtil } from '../common/utils/response.util';
+import { BadRequestException } from '@nestjs/common';
 
 const mockScheduleService = {
   create: jest.fn((dto) => {
     return { id: 1, ...dto };
   }),
   delete: jest.fn(),
+  getAll: jest.fn(),
 };
 
 describe('ScheduleController', () => {
   let controller: ScheduleController;
+  let responseUtil: ResponseUtil;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,10 +27,12 @@ describe('ScheduleController', () => {
           provide: ScheduleService,
           useValue: mockScheduleService,
         },
+        ResponseUtil,
       ],
     }).compile();
 
     controller = module.get<ScheduleController>(ScheduleController);
+    responseUtil = module.get<ResponseUtil>(ResponseUtil);
   });
 
   it('should be defined', () => {
@@ -55,6 +63,70 @@ describe('ScheduleController', () => {
       await controller.delete(id);
 
       expect(mockScheduleService.delete).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('getAll', () => {
+    it('should get all schedules for a valid page', async () => {
+      const query: GetScheduleQueryDTO = { page: '1' };
+      const expectedSchedules: Schedule[] = [
+        {
+          id: 'random-uuid',
+          hour: 10,
+          minute: 50,
+          userId: 'random-user-uuid',
+        },
+      ];
+      mockScheduleService.getAll.mockResolvedValue(expectedSchedules);
+
+      const schedules = await controller.getAll(query);
+
+      expect(schedules).toEqual(
+        responseUtil.response({}, { data: expectedSchedules }),
+      );
+      expect(mockScheduleService.getAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should return an empty array for a page beyond the total number of pages', async () => {
+      const query: GetScheduleQueryDTO = { page: '1000' };
+      const expectedSchedules: Schedule[] = [];
+      mockScheduleService.getAll.mockResolvedValue(expectedSchedules);
+
+      const schedules = await controller.getAll(query);
+
+      expect(schedules).toEqual(
+        responseUtil.response({}, { data: expectedSchedules }),
+      );
+      expect(mockScheduleService.getAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should return an error for invalid query parameters (page is not a number)', async () => {
+      const query: GetScheduleQueryDTO = { page: 'abc' };
+      mockScheduleService.getAll.mockRejectedValue(new BadRequestException());
+
+      expect(controller.getAll(query)).rejects.toThrow(BadRequestException);
+      expect(mockScheduleService.getAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should return an error for invalid query parameters (page is not a positive number)', async () => {
+      const query: GetScheduleQueryDTO = { page: '-1' };
+      mockScheduleService.getAll.mockRejectedValue(new BadRequestException());
+
+      await expect(controller.getAll(query)).rejects.toThrow(
+        BadRequestException,
+      );
+      expect(mockScheduleService.getAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should return an error if an unexpected error occurs in the service', async () => {
+      const query: GetScheduleQueryDTO = { page: '1' };
+      const expectedError = new Error('Unexpected error');
+      mockScheduleService.getAll.mockRejectedValue(expectedError);
+
+      await expect(controller.getAll(query)).rejects.toThrowError(
+        expectedError,
+      );
+      expect(mockScheduleService.getAll).toHaveBeenCalledWith(query);
     });
   });
 });
