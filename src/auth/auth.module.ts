ebe696fe1734690104bd94587826c386793d5b0d
marcellinuselbert <elbertmarcellinus@gmail.com>
2024-03-31 22:47:01 +0700

    [REFACTOR] remove unused imports in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,6 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { AuthMiddleware } from 'src/common/middleware/auth.middleware';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
