30aa022899e5ecd83299512d721db9320d038e2f
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-04-01 15:58:03 +0700

    [INFO] Create test for app.service.ts

---
+++ src/app.service.spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+
+const mockPrismaService = {
+  user: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+  },
+};
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AppService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('getHello should call prisma.user.findMany', async () => {
+    const prismaSpy = jest.spyOn(mockPrismaService.user, 'findMany');
+    await service.getHello();
+    expect(prismaSpy).toHaveBeenCalled();
+  });
+
+  it('getEnvironment should return local if no VERCEL_ENV is set', async () => {
+    delete process.env.VERCEL_ENV;
+    const environment = await service.getEnvironment();
+    expect(environment).toBe('local');
+  });
+
+  it('getEnvironment should return VERCEL_ENV if set', async () => {
+    process.env.VERCEL_ENV = 'production';
+    const environment = await service.getEnvironment();
+    expect(environment).toBe('production');
+  });
+
+  it('postUser should call prisma.user.create with email', async () => {
+    const email = 'test@example.com';
+    const prismaSpy = jest.spyOn(mockPrismaService.user, 'create');
+    await service.postUser(email);
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: { email },
+    });
+  });
+});
