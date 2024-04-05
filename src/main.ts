284eb6f011bcba8bcdb8e4d46faaedb7d58b7d0a
michaelsinanta <michaelchristlambert@gmail.com>
2024-04-06 03:27:38 +0700

    Fix Performance Monitoring with GlitchTip

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
   // GlitchTip Monitoring Setup
+  const client = new PrismaClient();
   const Sentry = require('@sentry/node');
   Sentry.init({
     dsn: process.env.SENTRY_DNS,
+    enableTracing: true,
     tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
     integrations: [
       ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+      new Sentry.Integrations.Prisma({ client }),
+      new Sentry.Integrations.Http({ tracing: true }),
     ],
   });
   app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
   app.use(Sentry.Handlers.errorHandler());
 
   await app.listen(process.env.PORT || 3000);
